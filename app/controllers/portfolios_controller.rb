a88da13b0f54fca62a77cd7594e7a618c05210f7
jackman365 <brapbrap86@googlemail.com>
2017-10-11 11:55:50 +0100

    Customised portfolio routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -35,6 +35,11 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def show
+   @portfolio_item = Portfolio.find(params[:id])
+
+  end
+
    def destroy
     @portfolio_item = Portfolio.find(params[:id])
 
